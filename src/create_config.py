e2efa1e80d9ba5d31144dd843e31c6f357d85b25
𝖺𝖻𝗎 <79229394+x64abu@users.noreply.github.com>
2026-09-25 15:17:58 +0330

    Update DNS
    Use dns via proxy as fallback

--- src/create_config.py
+++ src/create_config.py
@@ -15,7 +15,7 @@ def shadowrocket(bypass_domains: Iterable[str], ads_domains: Iterable[str]):
         "tun-excluded-routes = 10.0.0.0/8, 100.64.0.0/10, 127.0.0.0/8, 169.254.0.0/16, 172.16.0.0/12, 192.0.0.0/24, 192.0.2.0/24, 192.88.99.0/24, 192.168.0.0/16, 198.18.0.0/15, 198.51.100.0/24, 203.0.113.0/24, 224.0.0.0/4, 255.255.255.255/32\n"
         "always-ip-address = true\n"
         "dns-server = quic://9.9.9.9,tls://9.9.9.9,tls://1.1.1.1,tls://8.8.8.8,https://1.1.1.1/dns-query,https://8.8.8.8/dns-query\n"
-        "fallback-dns-server = quic://149.112.112.112,tls://149.112.112.112,tls://1.0.0.1,tls://8.8.4.4,https://1.0.0.1/dns-query,https://8.8.4.4/dns-query\n"
+        "fallback-dns-server = quic://9.9.9.9#proxy,tls://9.9.9.9#proxy,tls://1.1.1.1#proxy,tls://8.8.8.8#proxy,https://1.1.1.1/dns-query#proxy,https://8.8.8.8/dns-query#proxy\n"
         "dns-direct-system = true\n"
         "dns-direct-fallback-proxy = true\n"
         "ipv6 = true\n"
